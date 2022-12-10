611883a288908e2bdb148681d0e78351c595ee2a
eladron <63602693+eladron@users.noreply.github.com>
2022-12-10 14:23:37 +0200

    Added support for handling lists

--- Distill Module/Distilator.hpp
+++ Distill Module/Distilator.hpp
@@ -3,6 +3,7 @@
 
 #include "rapidxml.hpp"
 #include <string>
+#include <map>
 using namespace rapidxml;
 using namespace std;
 
@@ -12,9 +13,17 @@ class Distilator
         xml_document<> doc;
         xml_node<>* root;
         string file_name;
+        map<int, int> level_counters;
+        int list_level;
+        string file_text;
 
         string unzip_file();
         void extract_text();
+        void handle_text(xml_node<>* text_node);
+        void handle_paragraph_properties(xml_node<>* paragraph_node);
+        void handle_list(xml_node<>* pPr_node);
+        void handle_levels(int level);
+        void print_levels_counters();
 
     public:
         Distilator(char* file_name);
---
+++ Distill Module/Tags.h
@@ -0,0 +1,14 @@
+#ifndef TAGS_H
+#define TAGS_H
+
+#define PARAGRAPH "w:p"
+#define RUN "w:r"
+#define TEXT "w:t"
+#define PARAGRAPH_PROPERTY "w:pPr"
+#define PARAGRAPH_STYLE "w:pStyle"
+#define VALUE "w:val"
+#define NUM_PROPERTY "w:numPr"
+#define LEVEL "w:ilvl"
+#define LIST_PARAGRAPH "ListParagraph"
+#define NOT_IN_LIST -1
+#endif

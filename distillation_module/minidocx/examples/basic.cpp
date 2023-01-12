03eb5f524a3c1196b8671e737b01c3b48e93c8db
eladron <63602693+eladron@users.noreply.github.com>
2023-01-12 18:01:10 +0200

    Add files via upload

--- distillation_module/minidocx/examples/basic.cpp
+++ distillation_module/minidocx/examples/basic.cpp
@@ -10,7 +10,6 @@ int main()
   auto p1 = doc.AppendParagraph("Hello, World!", 12, "Times New Roman");
   auto p2 = doc.AppendParagraph(u8"אלעד רון", 14, "Ariel");
   p2.SetAlignment(Paragraph::Alignment::Right);
-  auto p3 = doc.AppendParagraph(u8"你好，World!", 16, "Times New Roman", u8"宋体");
 
   auto p4 = doc.AppendParagraph();
   p4.SetAlignment(Paragraph::Alignment::Centered);
@@ -18,10 +17,6 @@ int main()
   auto p4r1 = p4.AppendRun("This is a simple sentence. ", 12, "Arial");
   p4r1.SetCharacterSpacing(Pt2Twip(2));
 
-  auto p4r2 = p4.AppendRun(u8"这是一个简单的句子。");
-  p4r2.SetFontSize(14);
-  p4r2.SetFont(u8"黑体");
-  p4r2.SetFontStyle(Run::Bold | Run::Italic);
 
   doc.Save();
   return 0;

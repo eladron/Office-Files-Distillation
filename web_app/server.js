b150454983ed476fc1d4514f84e699259e33d10d
Eyal <eyal.semel@gmail.com>
2023-01-25 00:16:22 +0200

    updated web backend

--- web_app/server.js
+++ web_app/server.js
@@ -21,7 +21,7 @@ let i = 0;
 
 app.post('/api/upload', upload.single('file'), (req, res) => {
   const new_file_name = __dirname + `/uploads/document${i}.docx`;
-  const distilled_file_name = __dirname + `/distilled/document${i}.zip`;
+  const distilled_file_name = __dirname + `/distilled/document${i}.docx`;
   i++;
   fs.rename(req.file.path, new_file_name, function(err) {
     if (err) {
@@ -32,6 +32,10 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
       path.resolve(__dirname, '../distillation_module/distill')
   const args = [`${new_file_name}`, `${distilled_file_name}`]
   console.log('distilling file...')
+  console.log(distill_file_path)
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i])
+  }
   execFile(distill_file_path, args, (error, stdout, stderr) => {
     if (error === null) {
       let mailer = spawn(
